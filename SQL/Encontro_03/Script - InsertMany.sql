9ad4023703e1df1328466a34a73f0758f0e19a9f
Rafael Martins <rafaelgemelli1@hotmail.com>
2022-09-08 18:22:59 -0300

    Create Script - InsertMany.sql
    Inserindo dados a tabela Cliente.

---
+++ SQL/Encontro_03/Script - InsertMany.sql
@@ -0,0 +1,10 @@
+INSERT INTO Cliente(Nome,Documento,Telefone,Email,Endereco) // Inserindo dados a tabela Cliente.
+VALUES
+('Lucas','456','987654321','lucas@teste.com','Rua x, n. 11'),
+('João','789','987654564','joao@teste.com','Rua x, n. 12'),
+('Pedro','987','125496251','pedro@teste.com','Rua x, n. 13'),
+('Tiago','741','654133987','tiago@teste.com','Rua x, n. 14'),
+('Judas','256','852654745','judas@teste.com','Rua x, n. 15'),
+('Marcos','852','985254753','marcos@teste.com','Rua x, n. 16'),
+('André','527','426684789','andre@teste.com','Rua x, n. 17')
+GO
