a294e841d26285120c9f79cb98b4939edb9b8c13
Rafael Martins <rafaelgemelli1@hotmail.com>
2022-09-17 17:23:30 -0300

    Delete Estudo sobre Procedures.sql

--- SQL/Encontro_09/Estudo sobre Procedures.sql
+++
@@ -1,6 +0,0 @@
-DECLARE @SIGLA CHAR(2)
-SET @SIGLA = 'MS'
-SELECT CodigoMunicipio, NomeMunicipio, CodigoIBGE6, CodigoIBGE7, CEP, SiglaUF, DescricaoUF
-FROM VW_Municipios_IBGE
-WHERE (SiglaUF = @SIGLA)
-GO
