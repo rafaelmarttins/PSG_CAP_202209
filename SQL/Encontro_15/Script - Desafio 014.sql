cd2ab08400a4f48d591b4ae99de60d9bd7805b7b
Rafael Martins <rafaelgemelli1@hotmail.com>
2022-09-27 15:10:58 -0300

    Create Script - Desafio 014.sql
    Script responsável por mostrar nomes de uma tabela temporária.

---
+++ SQL/Encontro_15/Script - Desafio 014.sql
@@ -0,0 +1,47 @@
+--Desafio 014 – um professor quer armazenas o nome dos seus alunos para realizar algumas tarefas.
+--Faça um programa que ajude ele, lendo o nome deles e escrevendo todos os nomes
+--armazenados.
+
+--DECLARE @Nomes  
+--TABLE(NOME VARCHAR(MAX) NOT NULL)
+
+--INSERT INTO @Nomes VALUES
+--	('Daniel'),
+--	('Renato'),
+--	('Amanda'),
+--	('Rafael'),
+--	('Luiz'),
+--	('Bruno'),
+--	('Marlon'),
+--	('Aline'),
+--	('Jéssica'),
+--	('Ronie'),
+--	('Gérson')
+
+--SELECT * FROM @Nomes
+
+CREATE PROCEDURE SP_Desafio_014
+AS
+BEGIN
+	DECLARE @Nomes TABLE(NOME VARCHAR(MAX) NOT NULL)
+	
+	INSERT INTO @Nomes VALUES
+	('Daniel'),
+	('Renato'),
+	('Amanda'),
+	('Rafael'),
+	('Luiz'),
+	('Bruno'),
+	('Marlon'),
+	('Aline'),
+	('Jéssica'),
+	('Ronie'),
+	('Gérson')
+
+	SELECT * FROM @Nomes
+END 
+GO
+
+EXEC SP_Desafio_014
+GO
+
